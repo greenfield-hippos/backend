1696def08bad1cc6c283cbbee18b2e318a446e95
Jason Sagalow <jrssagalow@gmail.com>
2024-10-23 12:21:33 +0900

    fix: salt and hash columns to salted_hash column

--- db/migrations/20241022064916_create_table_chat_user.js
+++ db/migrations/20241022064916_create_table_chat_user.js
@@ -6,8 +6,7 @@ exports.up = async function(knex) {
   await knex.schema.createTable('chat_user', (table) => {
     table.increments('id').primary();
     table.string('username').notNullable().unique();
-    table.string('hash').notNullable();
-    table.string('salt').notNullable();
+    table.string('salted_hash').notNullable();
     table.boolean('isAdmin').notNullable();
     table.timestamp('account_created').defaultTo(knex.fn.now());
     table.timestamp('last_login');
