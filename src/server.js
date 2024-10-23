fe5341378b256188dc55211ed87b548fefa2f6fd
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-10-23 11:08:44 +0900

    feat: installs bcrypt and adds authentication f()

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const knex = require('./knex');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
@@ -10,6 +11,27 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//for new user creation
+async function hashPassword(plainTextPassword) {
+  const saltRounds = 10; //the higher the more secure but more time-consuming
+  try {
+      const hash = await bcrypt.hash(plainTextPassword, saltRounds); //applies salt and hashes the password
+      return hash;
+  } catch (err) {
+      console.error('Hashing error:', err);
+  }
+}
+
+//for existing user login
+async function verifyPassword(plainTextPassword, hashedPasswordFromDB) {
+  try {
+      const match = await bcrypt.compare(plainTextPassword, hashedPasswordFromDB); //the salt can be implicitly extracted from the hashed password
+      return match;
+  } catch (err) {
+      console.error('Verification error:', err);
+  }
+}
 
 app.listen(PORT, () =>{
   console.log(`Express server is up and running on ${PORT}`);
