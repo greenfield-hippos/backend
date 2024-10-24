8da8227126919dc3db64dc62bdb4c12126ed3385
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-10-24 11:23:34 +0900

    Feat: Adds crypto and express-sessions

--- src/server.js
+++ src/server.js
@@ -4,12 +4,23 @@ const app = express();
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const openaiRequest = require('./openairequest');
-
+const session = require('express-session')
+const crypto = require('crypto');
+const sessionSecret = process.env.SESSION_SECRET || crypto.randomBytes(64).toString('hex');
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(session({
+  secret: sessionSecret, 
+  resave: false,
+  saveUninitialized: false, // Sample used true, but seems unnecessary for our use case
+  cookie: { secure: false } // Set to true if using HTTPS
+}));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
